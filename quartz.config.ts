1ad062ae96332804740f607a534fce82d7cca50b
txx-macmini <tangxinxu88@gmail.com>
2025-06-13 22:59:14 +0800

    清空页面标题后缀

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "MonkeyHaven🐒",
-    pageTitleSuffix: "试着勇敢一些",
+    pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
